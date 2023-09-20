a89c14915ab63d614bb6773a017f7b0d6fa8ec39
Felipe <felipe.baleche2@gmail.com>
2023-09-20 20:17:35 -0300

    finished

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# ProjetoC-PUCPR
+# Programação Imperativa – Trabalho 1
+## Prof. Alcides Calsavara – PUCPR
+
+Uma loja possui um catálogo de produtos e uma lista de vendedores, sendo que:
+
+- Cada produto é caracterizado por:
+  - Um código (valor inteiro)
+  - Um preço (valor real)
+  - Uma descrição (uma string) – basta uma palavra
+
+- Cada vendedor é caracterizado por:
+  - Um código (valor inteiro)
+  - Um nome (uma string) – basta uma palavra
+
+Todas as vendas da loja em cada dia são registradas em um arquivo de “log”, sendo que cada linha do arquivo contém o seguinte:
+
+- O código do vendedor que fez a venda
+- O código do produto vendido
+- O número de unidades vendidas do produto
+
+Por exemplo, a linha com o conteúdo `54 89 4` indica a venda de 4 unidades do produto 89 pelo vendedor 54.
+
+A loja tem interesse de, ao final do dia, saber o valor total geral vendido, bem como o valor total vendido por cada produto e, ainda, o valor total vendido por cada vendedor.
+
+Escreva um programa na Linguagem C que:
+
+1. Armazene em memória o log de vendas a partir de dados lidos de um arquivo texto denominado `vendas.txt`.
+2. Armazene em memória o catálogo de produtos a partir de dados lidos de um arquivo texto denominado `produtos.txt`.
+3. Armazene em memória a lista de vendedores a partir de dados lidos de um arquivo texto denominado `vendedores.txt`.
+4. Calcule o total geral das vendas do dia, bem como o total vendido por produto e o total vendido por cada vendedor.
+5. Grave todos os dados lidos e os totais calculados em um arquivo texto denominado `totais.txt`.
+
+Os arquivos de entrada devem ter exatamente a forma mostrada no exemplo a seguir. Deve-se observar que a primeira linha de cada arquivo informa a quantidade de linhas restantes no arquivo, isto é, a quantidade de linhas efetivamente de dados. Cada linha de dados de um arquivo tem a seguinte forma:
+
+- Arquivo `vendas.txt`:
+  - código-do-vendedor código-do-produto unidades-vendidas
+
+- Arquivo `produtos.txt`:
+  - código-do-produto preço-do-produto descrição-do-produto
+
+- Arquivo `vendedores.txt`:
+  - código-do-vendedor nome-do-vendedor
+
+Cada equipe de estudantes deverá criar os seus próprios arquivos de entrada, sendo o volume mínimo de dados de acordo com a seguinte tabela:
+
+Arquivo | Número mínimo de linhas com dados
+--- | ---
+vendas.txt | 20
+produtos.txt | 10
+vendedores.txt | 5
+
+Sugere-se usar os arquivos de entrada mostrados no exemplo a seguir para fins de validação do programa.
+
+Na apresentação e defesa do programa, deverão ser usados os arquivos criados pela equipe de estudantes.
+
--- main.c
+++ main.c
@@ -5,24 +5,30 @@ typedef struct
 {
     int id;
     float price;
+    double amountSold;
 } product;
 
 typedef struct
 {
     int id;
+    double totalSales;
 } seller;
 
 int main()
 {
+
+    system("clear");
+
     FILE *productsFile = fopen("files/produtos.txt", "r");
     FILE *sellersFile = fopen("files/vendedores.txt", "r");
     FILE *salesFile = fopen("files/vendas.txt", "r");
+    FILE *totalsFile = fopen("files/totais.txt", "w");
 
-    int productsLines;
-    int sellersLines;
+    int productsLines, sellersLines, salesLines;
 
     fscanf(productsFile, "%d", &productsLines);
     fscanf(sellersFile, "%d", &sellersLines);
+    fscanf(salesFile, "%d", &salesLines);
 
     product products[productsLines];
     seller sellers[sellersLines];
@@ -30,21 +36,85 @@ int main()
     int id;
     int counter = 0;
     double price;
-    char name[20];
+    char name[15];
 
+    fprintf(totalsFile, "Catálogo de produtos:\n");
     while (fscanf(productsFile, "%i%lf%s", &id, &price, name) != -1)
     {
+        fprintf(totalsFile, "[%d] %d %.2f %s\n", counter, id, price, name);
         products[counter].id = id;
         products[counter].price = price;
+        products[counter].amountSold = 0;
         counter += 1;
     }
 
     counter = 0;
 
+    fprintf(totalsFile, "\nLista de vendedores:\n");
     while (fscanf(sellersFile, "%i%s", &id, name) != -1)
     {
+        fprintf(totalsFile, "[%d] %d %s\n", counter, id, name);
         sellers[counter].id = id;
+        sellers[counter].totalSales = 0;
         counter += 1;
+    }
+
+    counter = 0;
+
+    int sellerID, productID, quantity;
+    double total = 0;
+
+    fprintf(totalsFile, "\nLog de Vendas:\n");
+    while (fscanf(salesFile, "%d%d%d", &sellerID, &productID, &quantity) != -1)
+    {
+        fprintf(totalsFile, "[%d] %d %d %d\n", counter, sellerID, productID, quantity);
+        for (int i = 0; i < productsLines; i++)
+        {
+            if (products[i].id == productID)
+            {
+                total += products[i].price * quantity;
+                products[i].amountSold += quantity * products[i].price;
+            }
+        }
+
+        for (int i = 0; i < sellersLines; i++)
+        {
+            if (sellers[i].id == sellerID)
+            {
+                for (int j = 0; j < productsLines; j++)
+                {
+                    if (products[j].id == productID)
+                    {
+                        sellers[i].totalSales += products[j].price * quantity;
+                    }
+                }
+            }
+        }
+        counter++;
+    }
+
+    fprintf(totalsFile, "\nTotal geral vendido: %.2f\n", total);
+
+    rewind(productsFile);
+    fscanf(productsFile, "%d", &productsLines);
+    counter = 0;
+
+    fprintf(totalsFile, "\nTotal de vendas de cada produto:\n");
+    while (fscanf(productsFile, "%d%lf%s", &id, &price, name) != -1)
+    {
+        fprintf(totalsFile, "Produto %d (%s): R$%.2lf\n", id, name, products[counter].amountSold);
+        counter++;
+    }
+
+    rewind(sellersFile);
+    fscanf(sellersFile, "%d", &sellersLines);
+    counter = 0;
+
+    fprintf(totalsFile, "\nTotal de vendas de cada vendedor:\n");
+    while (fscanf(sellersFile, "%d%s", &id, name) != -1)
+    {
+        fprintf(totalsFile, "Vendedor %d (%s): R$%.2lf\n", id, name, sellers[counter].totalSales);
+        counter++;
     }
 
     return 0;
